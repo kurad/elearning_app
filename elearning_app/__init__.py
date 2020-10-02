96a2c20b5d787fe588b2c5ccadf12e5fbdef56a1
Benjamin Kuradusenge <bendusenge@gmail.com>
2020-10-02 12:37:34 +0200

    Add project files.

---
+++ elearning_app/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for elearning_app.
+"""
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\GGAST-115\source\repos\elearning_app\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
